50cf3d8d2a08800b242af104131e70bbdb64b80d
Jeremy Petter <jeremy.petter@gmail.com>
2015-04-20 18:58:32 -0700

    tries to deal with bogus user input

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -11,20 +11,26 @@
 int main() {
     int Fuzz = 5;
     int Bizz = 7;
-    int Nezz= 387;
+    int Nezz = 0;
     
-    int count = 0;
-    while (count < Nezz){
-        count++;
-        if (count % Fuzz == 0 || count % Bizz == 0){
-            if (count % Fuzz == 0)
-                printf("Fuzz");
-            if (count % Bizz == 0)
-                printf("Bizz");
-        }else{
-            printf("%d",count);
+    // The sizeof clause isn't excluding non-ints entered into Nezz as I'd hope it would :(
+    
+    if (sizeof(Nezz) == 4 && Nezz >0){
+        int count = 0;
+        while (count < Nezz){
+            count++;
+            if (count % Fuzz == 0 || count % Bizz == 0){
+                if (count % Fuzz == 0)
+                    printf("Fuzz");
+                if (count % Bizz == 0)
+                    printf("Bizz");
+            }else{
+                printf("%d",count);
+            }
+            printf("\n");
         }
-        printf("\n");
+    } else {
+        printf("Nezz is not a real number.");
     }
         return 0;
 }
